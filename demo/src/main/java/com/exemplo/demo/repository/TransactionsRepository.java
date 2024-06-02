28a6c4ba32c97f81fb676c989a380cbfcc391266
Rafael B. Zanon <zanonrafael42@gmail.com>
2024-06-02 18:40:22 -0300

    fix: fixed TransactionsRepository

--- demo/src/main/java/com/exemplo/demo/repository/TransactionsRepository.java
+++ demo/src/main/java/com/exemplo/demo/repository/TransactionsRepository.java
@@ -1,7 +1,7 @@
 package com.exemplo.demo.repository;
 
-import jakarta.transaction.Transaction;
+import com.exemplo.demo.models.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionsRepository extends JpaRepository<Transaction, Integer> {
+public interface TransactionsRepository extends JpaRepository<Transactions, Integer> {
 }
