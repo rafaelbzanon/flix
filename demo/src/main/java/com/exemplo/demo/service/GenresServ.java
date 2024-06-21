2d3fffe541ab7450b4881f6e40494d1f72e0f84d
Gabriel Anceski <gabrielanceski@gmail.com>
2024-06-21 15:30:48 -0300

    fix: genre name validation.

--- demo/src/main/java/com/exemplo/demo/service/GenresServ.java
+++ demo/src/main/java/com/exemplo/demo/service/GenresServ.java
@@ -3,7 +3,9 @@ package com.exemplo.demo.service;
 import com.exemplo.demo.dto.GenreDTO;
 import com.exemplo.demo.entity.Genres;
 import com.exemplo.demo.repository.GenresRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class GenresServ {
@@ -14,8 +16,14 @@ public class GenresServ {
     }
 
     public Genres createOrSave(GenreDTO dto) {
-        Genres genre = (dto.getName() == null) ? new Genres() :
-                repository.findByName(dto.getName()).orElseGet(Genres::new);
+        // Valida se o gênero possui um nome válido.
+        if (dto.getName() == null || dto.getName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Nome do Gênero não existe.");
+        }
+
+        // Busca o gênero informado, se não encontrar crie um novo.
+        Genres genre = repository.findByName(dto.getName()).orElseGet(Genres::new);
+        genre.setName(dto.getName());
         repository.save(genre);
         return genre;
     }
